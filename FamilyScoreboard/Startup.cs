91783264a0ae6d7aa42f99721c736d3c7b3d30d2
Michael S North <mikesnorth@gmail.com>
2020-02-01 11:47:14 -0500

    Adds swagger

--- FamilyScoreboard/Startup.cs
+++ FamilyScoreboard/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using FamilyScoreboard.DataModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace FamilyScoreboard
 {
@@ -29,6 +30,27 @@ namespace FamilyScoreboard
         {
             services.AddDbContext<FamilyScoreboardContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DbContext")));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Family Scoreboard API",
+                    Description = "",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "God",
+                        Email = "God@heaven.com",
+                        Url = new Uri("http://www.IReallyHopeYouGetThisThisIsFakeContactInfo.net")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = new Uri("https://github.com/Mikesnorth/FamilyScoreboard/blob/master/LICENSE")
+                    },
+                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +70,12 @@ namespace FamilyScoreboard
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Family Scoreboard V1");
             });
         }
     }
