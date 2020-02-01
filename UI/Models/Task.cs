bd8b7ff36904aaa25e8c25ef4c08c08d30da9e25
Michael S North <mikesnorth@gmail.com>
2020-02-01 18:15:07 -0500

    adds razor ui project

---
+++ UI/Models/Task.cs
@@ -0,0 +1,9 @@
+﻿
+namespace UI.Models {
+    public class Task {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int PointValue { get; set; }
+    }
+}
