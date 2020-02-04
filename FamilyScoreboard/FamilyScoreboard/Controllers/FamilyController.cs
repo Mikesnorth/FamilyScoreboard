27a509a9dffcd59ca7979d8d4dcd241c90783df6
Michael S North <mikesnorth@gmail.com>
2020-02-04 17:17:12 -0500

    committing progress

--- FamilyScoreboard/FamilyScoreboard/Controllers/FamilyController.cs
+++ FamilyScoreboard/FamilyScoreboard/Controllers/FamilyController.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using FamilyScoreboard.Infrastructure;
 using FamilyScoreboard.Models;
 using FamilyScoreboard.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -51,9 +53,39 @@ namespace FamilyScoreboard.Controllers {
             }
 
             var model = new Family {
-                FamilyMembers = _dbContext.FamilyMemebers.ToList()
+                FamilyMembers = _dbContext.FamilyMemebers.Include(fm => fm.CompletedChores).ToList()
             };
             return View("Index", model);
+        }
+
+        public ActionResult Details(int id) {
+            var model = new FamilyMemberDetails {
+                FamilyMember = _dbContext.FamilyMemebers.Include(fm => fm.CompletedChores).Single(_ => _.Id == id),
+                Chores = _dbContext.Chores.ToList()
+            };
+            return View(model);
+        }
+
+        public ActionResult MarkChoreComplete(FamilyMemberDetails memberDetails) {
+            var familyMemeber = _dbContext.FamilyMemebers.Include(fm => fm.CompletedChores).Single(_ => _.Id == memberDetails.FamilyMember.Id);
+            var choreToMark = _dbContext.Chores.Single(_ => _.Id == memberDetails.SelectedChoreId);
+
+            if(familyMemeber.CompletedChores == null) {
+                familyMemeber.CompletedChores = new List<CompletedChore>();
+            }
+            familyMemeber.CompletedChores.Add(new CompletedChore {
+                DateEntered = DateTimeOffset.Now,
+                PointsEarned = choreToMark.PointValue,
+                Chore = choreToMark
+            });
+
+            _dbContext.SaveChanges();
+
+            var model = new FamilyMemberDetails{
+                FamilyMember = _dbContext.FamilyMemebers.Single(_ => _.Id == memberDetails.FamilyMember.Id),
+                Chores = _dbContext.Chores.ToList()
+            };
+            return View("Details", model);
         }
     }
 }
---
+++ FamilyScoreboard/FamilyScoreboard/ViewModels/FamilyMemberDetails.cs
@@ -0,0 +1,13 @@
+﻿using FamilyScoreboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyScoreboard.ViewModels {
+    public class FamilyMemberDetails {
+        public FamilyMember FamilyMember { get; set; }
+        public IEnumerable<Chore> Chores { get; set; }
+        public int SelectedChoreId { get; set; }
+    }
+}
