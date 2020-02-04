baa7a96262754f3293da7e8162bef46682fe0557
Michael S North <mikesnorth@gmail.com>
2020-02-04 08:27:02 -0500

    really clean house

--- UI/Models/Task.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace UI.Models {
-    public class Task {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int PointValue { get; set; }
-    }
-}
