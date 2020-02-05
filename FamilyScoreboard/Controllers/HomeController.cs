6d8d3b4580b4dfd17588e80ac504e3bf9b688dc0
Michael S North <mikesnorth@gmail.com>
2020-02-05 18:55:27 -0500

    adds basic dashboard with no styling

--- FamilyScoreboard/Controllers/HomeController.cs
+++ FamilyScoreboard/Controllers/HomeController.cs
@@ -6,17 +6,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FamilyScoreboard.Models;
+using FamilyScoreboard.ViewModels;
+using FamilyScoreboard.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 namespace FamilyScoreboard.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
+        private readonly ScoreboardContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger) {
+        public HomeController(ILogger<HomeController> logger, ScoreboardContext dbContext) {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
-        public IActionResult Index() {
-            return View();
+        public ActionResult Index() {
+            var model = new Dashboard {
+                FamilyMemebers = _dbContext.FamilyMemebers.Include(_ => _.CompletedChores).ToList()
+            };
+            model.TotalPointsThisWeek = model.FamilyMemebers.Sum(CalculatePointsThisWeek);
+            model.TotalPointsThisMonth = model.FamilyMemebers.Sum(CalculatePointsThisMonth);
+            model.ThisWeeksLeader = model.FamilyMemebers.OrderByDescending(CalculatePointsThisWeek).First();
+            model.ThisMonthsLeader = model.FamilyMemebers.OrderByDescending(CalculatePointsThisMonth).First();
+            return View(model);
         }
 
         public IActionResult Privacy() {
@@ -26,6 +38,16 @@ namespace FamilyScoreboard.Controllers {
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private int CalculatePointsThisWeek(FamilyMember familyMember) {
+            var completedChoresThisWeek = familyMember.CompletedChores.Where(_ => _.DateEntered > DateTimeOffset.Now.AddDays(-7));
+            return completedChoresThisWeek.Sum(_ => _.PointsEarned);
+        }
+
+        private int CalculatePointsThisMonth(FamilyMember familyMember) {
+            var completedChoresThisWeek = familyMember.CompletedChores.Where(_ => _.DateEntered.Month == DateTimeOffset.Now.Month && _.DateEntered.Year == DateTimeOffset.Now.Year);
+            return completedChoresThisWeek.Sum(_ => _.PointsEarned);
         }
     }
 }
---
+++ FamilyScoreboard/ViewModels/Dashboard.cs
@@ -0,0 +1,17 @@
+﻿using FamilyScoreboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyScoreboard.ViewModels {
+    public class Dashboard {
+        public IList<FamilyMember> FamilyMemebers { get; set; }
+
+        public FamilyMember ThisMonthsLeader { get; set; }
+        public FamilyMember ThisWeeksLeader { get; set; }
+        public int TotalPointsThisWeek { get; set; }
+        public int TotalPointsThisMonth { get; set; }
+
+    }
+}
